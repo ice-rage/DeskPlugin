068c3974ca6d98c2d77feda22b0230f496799254
ice-rage <otulissa2012@yandex.ru>
2022-09-21 19:23:04 +0700

    feature(DeskParameters): добавлены перечисления типов параметров модели и их групп

---
+++ src/DeskPlugin/DeskParameters/Enums/ParameterGroupType.cs
@@ -0,0 +1,23 @@
+﻿namespace DeskParameters.Enums
+{
+    /// <summary>
+    /// Перечисление, содержащее типы групп параметров письменного стола.
+    /// </summary>
+    public enum ParameterGroupType
+    {
+        /// <summary>
+        /// Столешница.
+        /// </summary>
+        Worktop,
+
+        /// <summary>
+        /// Ножки.
+        /// </summary>
+        Legs,
+
+        /// <summary>
+        /// Ящики для канцелярии.
+        /// </summary>
+        Drawers
+    }
+}
---
+++ src/DeskPlugin/DeskParameters/Enums/ParameterType.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel;
+
+namespace DeskParameters.Enums
+{
+    /// <summary>
+    /// Перечисление, содержащее типы параметров письменного стола.
+    /// </summary>
+    public enum ParameterType
+    {
+        /// <summary>
+        /// Длина столешницы.
+        /// </summary>
+        [Description("Length (L1)")]
+        WorktopLength,
+
+        /// <summary>
+        /// Ширина столешницы.
+        /// </summary>
+        [Description("Width (B)")]
+        WorktopWidth,
+
+        /// <summary>
+        /// Высота столешницы.
+        /// </summary>
+        [Description("Height (H1)")]
+        WorktopHeight,
+
+        /// <summary>
+        /// Диаметр основания ножек.
+        /// </summary>
+        [Description("Base Diameter (D)")]
+        LegBaseDiameter,
+
+        /// <summary>
+        /// Длина основания ножек.
+        /// </summary>
+        [Description("Base Length (A)")]
+        LegBaseLength,
+
+        /// <summary>
+        /// Высота ножек.
+        /// </summary>
+        [Description("Height (H2)")]
+        LegHeight,
+
+        /// <summary>
+        /// Количество ящиков для канцелярии.
+        /// </summary>
+        [Description("Number")]
+        DrawerNumber,
+
+        /// <summary>
+        /// Длина ящиков для канцелярии.
+        /// </summary>
+        [Description("Length (L2)")]
+        DrawerLength
+    }
+}
