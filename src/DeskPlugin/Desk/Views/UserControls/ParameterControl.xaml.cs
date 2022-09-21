a54448f78bd1bc566e69d977e09ba1840c49755c
ice-rage <otulissa2012@yandex.ru>
2022-09-21 22:33:23 +0700

    fix(Desk): перенос некоторой части разметки главного окна ParameterControl в code-behind

--- src/DeskPlugin/Desk/Views/UserControls/ParameterControl.xaml.cs
+++ src/DeskPlugin/Desk/Views/UserControls/ParameterControl.xaml.cs
@@ -1,7 +1,17 @@
-﻿namespace Desk.Views.UserControls
+﻿using Microsoft.Xaml.Behaviors;
+using System.Windows;
+
+namespace Desk.Views.UserControls
 {
     public partial class ParameterControl
     {
-        public ParameterControl() => InitializeComponent();
+        public ParameterControl()
+        {
+            InitializeComponent();
+
+            //TODO: костыль?..
+            var _ = new DefaultTriggerAttribute(typeof(Trigger),
+                typeof(Microsoft.Xaml.Behaviors.TriggerBase), null);
+        }
     }
 }
--- src/DeskPlugin/Desk/Views/Windows/MainWindow.xaml.cs
+++ src/DeskPlugin/Desk/Views/Windows/MainWindow.xaml.cs
@@ -1,7 +1,25 @@
-﻿namespace Desk.Views.Windows
+﻿using DeskParameters.Enums;
+using DeskViewModel;
+using Microsoft.Xaml.Behaviors;
+using System.Windows;
+using System;
+
+namespace Desk.Views.Windows
 {
     public partial class MainWindow
     {
-        public MainWindow() => InitializeComponent();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //TODO: костыли?..
+            var _ = new DefaultTriggerAttribute(typeof(Trigger), 
+                typeof(Microsoft.Xaml.Behaviors.TriggerBase), null);
+
+            DataContext = new MainWindowViewModel();
+
+            Array legTypeEnumDataSource = Enum.GetValues(typeof(LegType));
+            LegTypeComboBox.ItemsSource = legTypeEnumDataSource;
+        }
     }
 }
