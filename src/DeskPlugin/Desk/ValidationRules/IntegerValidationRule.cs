7264bb9bf467db23d0eac0b9187c82638acf276d
ice-rage <otulissa2012@yandex.ru>
2022-09-21 19:36:50 +0700

    feature(Desk): добавлены правила валидации ввода параметров (проверка на пустоту/пробелы, проверка на целочисленность)

---
+++ src/DeskPlugin/Desk/ValidationRules/IntegerValidationRule.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace Desk.ValidationRules
+{
+    /// <summary>
+    /// Правило проверки пользовательского ввода на целые числа.
+    /// </summary>
+    public class IntegerValidationRule : ValidationRule
+    {
+        /// <inheritdoc/>
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            var stringValue = value as string;
+
+            return int.TryParse(stringValue, out int _) 
+                ? new ValidationResult(true, null)
+                : new ValidationResult(false, "Value must be an integer");
+        }
+    }
+}
---
+++ src/DeskPlugin/Desk/ValidationRules/NullOrWhiteSpaceValidationRule.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace Desk.ValidationRules
+{
+    /// <summary>
+    /// Правило проверки пользовательского ввода на пустоту или заполненность только пробелами.
+    /// </summary>
+    public class NullOrWhiteSpaceValidationRule : ValidationRule
+    {
+        /// <inheritdoc/>
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            var stringValue = value as string;
+
+            return string.IsNullOrWhiteSpace(stringValue) 
+                ? new ValidationResult(false, "Value may not be blank or white-spaces") 
+                : ValidationResult.ValidResult;
+        }
+    }
+}
