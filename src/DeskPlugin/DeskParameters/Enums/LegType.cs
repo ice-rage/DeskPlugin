47b3a4670bbd90e16a3b839d5b1455ab1613c637
ice-rage <otulissa2012@yandex.ru>
2022-09-21 18:16:13 +0700

    feature(DeskParameters): добавлено перечисление типов ножек стола

---
+++ src/DeskPlugin/DeskParameters/Enums/LegType.cs
@@ -0,0 +1,18 @@
+﻿namespace DeskParameters.Enums
+{
+    /// <summary>
+    /// Перечисление, содержащее типы ножек стола.
+    /// </summary>
+    public enum LegType
+    {
+        /// <summary>
+        /// Круглые ножки.
+        /// </summary>
+        Round,
+
+        /// <summary>
+        /// Квадратные ножки.
+        /// </summary>
+        Square
+    }
+}
