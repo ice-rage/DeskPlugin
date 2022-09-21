057ce741c7135db69507359e60a1ab792ac176f8
ice-rage <otulissa2012@yandex.ru>
2022-09-21 21:50:30 +0700

    feature(AutoCadConnector): добавлен класс для взаимодействия с AutoCAD

---
+++ src/DeskPlugin/AutoCadConnector/Connector.cs
@@ -0,0 +1,25 @@
+﻿using Autodesk.AutoCAD.Runtime;
+using Desk.Views.Windows;
+using Application = Autodesk.AutoCAD.ApplicationServices.Core.Application;
+
+namespace AutoCadConnector
+{
+    /// <summary>
+    /// Класс, предназначенный для взаимодействия AutoCAD с плагином.
+    /// </summary>
+    public class Connector : IExtensionApplication
+    {
+        [CommandMethod("StartDeskPlugin")]
+        public static void StartDeskPlugin() => Application.ShowModelessWindow(new MainWindow());
+
+        /// <inheritdoc/>
+        public void Initialize()
+        {
+        }
+
+        /// <inheritdoc/>
+        public void Terminate()
+        {
+        }
+    }
+}
