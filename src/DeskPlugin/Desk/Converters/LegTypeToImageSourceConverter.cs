a2af45071afa2578c30c4e428aa02d9fadf8a68f
ice-rage <otulissa2012@yandex.ru>
2022-09-21 18:19:20 +0700

    feature(Desk): добавлен конвертер, преобразующий тип ножек стола в соответствующее изображение модели

---
+++ src/DeskPlugin/Desk/Converters/LegTypeToImageSourceConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+using DeskParameters.Enums;
+
+namespace Desk.Converters
+{
+    /// <summary>
+    /// Конвертер для преобразования выбранного типа ножек письменного стола в соответствующее
+    /// изображение его 3D-модели.
+    /// </summary>
+    [ValueConversion(typeof(LegType), typeof(string))]
+    [MarkupExtensionReturnType(typeof(LegTypeToImageSourceConverter))]
+    public class LegTypeToImageSourceConverter : IValueConverter
+    {
+        /// <inheritdoc/>
+        public object Convert(object value, Type targetType, object parameter,
+            CultureInfo culture)
+        {
+            switch (value)
+            {
+                case LegType.Round:
+                {
+                    return "../../Resources/DeskWithRoundLegs.png";
+                }
+                case LegType.Square:
+                {
+                    return "../../Resources/DeskWithSquareLegs.png";
+                }
+                default:
+                {
+                    return null;
+                }
+            }
+        }
+
+        /// <inheritdoc/>
+        public object ConvertBack(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
