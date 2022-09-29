a5828159cd2bf5436677e2cf73bc3eca6632e7df
ice-rage <otulissa2012@yandex.ru>
2022-09-29 10:49:09 +0700

    feature(TestParameters): написаны модульные тесты для классов-расширений перечислений

--- src/DeskPlugin/Testing/TestParameters/DeskParameterTypeExtensionTests.cs
+++ src/DeskPlugin/Testing/TestParameters/DeskParameterTypeExtensionTests.cs
@@ -1,10 +1,46 @@
 ﻿using NUnit.Framework;
+using Parameters.Enums;
+using Parameters.Enums.Extensions;
 
 namespace TestParameters
 {
+    /// <summary>
+    /// Класс, содержащий модульные тесты для класса <see cref="DeskParameterTypeExtension"/>.
+    /// </summary>
     [TestFixture]
     public class DeskParameterTypeExtensionTests
     {
+        /// <summary>
+        /// Название модульного теста для метода
+        /// <see cref="DeskParameterTypeExtension.GetDescription"/>.
+        /// </summary>
+        private const string TestGetDescription_ReturnsValue_TestName = "Когда вызывается метод " + 
+            "GetDescription() для параметра {0}, он должен вернуть строку {1}";
 
+        [TestCase(DeskParameterType.WorktopLength, "Length (L1)",
+            TestName = TestGetDescription_ReturnsValue_TestName)]
+        [TestCase(DeskParameterType.WorktopWidth, "Width (B)",
+            TestName = TestGetDescription_ReturnsValue_TestName)]
+        [TestCase(DeskParameterType.WorktopHeight, "Height (H1)",
+            TestName = TestGetDescription_ReturnsValue_TestName)]
+        [TestCase(DeskParameterType.LegBaseDiameter, "Base Diameter (D)",
+            TestName = TestGetDescription_ReturnsValue_TestName)]
+        [TestCase(DeskParameterType.LegBaseLength, "Base Length (A)",
+            TestName = TestGetDescription_ReturnsValue_TestName)]
+        [TestCase(DeskParameterType.LegHeight, "Height (H2)",
+            TestName = TestGetDescription_ReturnsValue_TestName)]
+        [TestCase(DeskParameterType.DrawerNumber, "Number",
+            TestName = TestGetDescription_ReturnsValue_TestName)]
+        [TestCase(DeskParameterType.DrawerLength, "Length (L2)",
+            TestName = TestGetDescription_ReturnsValue_TestName)]
+        public void TestGetDescription_ReturnsValue(DeskParameterType parameterType, 
+            string expected)
+        {
+            // Act
+            string actual = parameterType.GetDescription();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
--- src/DeskPlugin/Testing/TestParameters/LegTypeExtensionTests.cs
+++ src/DeskPlugin/Testing/TestParameters/LegTypeExtensionTests.cs
@@ -1,10 +1,32 @@
 ﻿using NUnit.Framework;
+using Parameters.Enums;
+using Parameters.Enums.Extensions;
 
 namespace TestParameters
 {
+    /// <summary>
+    /// Класс, содержащий модульные тесты для класса <see cref="LegTypeExtensionTests"/>.
+    /// </summary>
     [TestFixture]
     public class LegTypeExtensionTests
     {
+        /// <summary>
+        /// Название модульного теста для метода <see cref="LegTypeExtensionTests"/>.
+        /// </summary>
+        private const string TestGetLegBaseType_ReturnsValue_TestName = "Когда вызывается метод " + 
+            "GetLegBaseType() для параметра {1}, он должен вернуть соответствующий параметр {0}";
 
+        [TestCase(DeskParameterType.LegBaseDiameter, LegType.Round, TestName = 
+            TestGetLegBaseType_ReturnsValue_TestName)]
+        [TestCase(DeskParameterType.LegBaseLength, LegType.Square, TestName = 
+            TestGetLegBaseType_ReturnsValue_TestName)]
+        public void TestGetLegBaseType_ReturnsValue(DeskParameterType expected, LegType legType)
+        {
+            // Act
+            DeskParameterType actual = legType.GetLegBaseType();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
