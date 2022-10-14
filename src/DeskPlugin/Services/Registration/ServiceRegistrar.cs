cec9a5468140237a53fe59b6419367b93b04010b
ice-rage <otulissa2012@yandex.ru>
2022-10-14 14:25:04 +0700

    feature(Services): регистрация сервисов плагина в контейнере сервисов

---
+++ src/DeskPlugin/Services/Registration/ServiceRegistrar.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Services.Interfaces;
+
+namespace Services.Registration
+{
+    /// <summary>
+    /// Статический класс для регистрации сервисов, используемых плагином,
+    /// в контейнере сервисов.
+    /// </summary>
+    public static class ServiceRegistrar
+    {
+        /// <summary>
+        /// Метод, регистрирующий сервисы и определяющий их жизненный цикл в рамках
+        /// выполнения плагина.
+        /// </summary>
+        /// <param name="services"> Указанный контейнер сервисов.</param>
+        /// <returns> Коллекция сервисов плагина с добавленными сервисами.</returns>
+        public static IServiceCollection RegisterServices(
+            this IServiceCollection services) => services
+            .AddSingleton<ICadWrapper>(_ => new AutoCadWrapper("Desk"));
+    }
+}
