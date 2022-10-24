6632b9b2c240fcc6bcdae15ebec82b07d08acd3c
ice-rage <otulissa2012@yandex.ru>
2022-10-24 14:04:43 +0700

    feature(Desk): добавлено расширение разметки для использования перечислений в XAML

---
+++ src/DeskPlugin/Desk/Extensions/MarkupExtensions/EnumValuesExtension.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace Desk.Extensions.MarkupExtensions
+{
+    /// <summary>
+    /// Класс, предоставляющий значения некоторого перечисления по его типу.
+    /// <para> Наследуется от класса <see cref="MarkupExtension"/> с целью
+    /// использования данного класса непосредственно в разметке окон.</para>
+    /// </summary>
+    internal class EnumValuesExtension : MarkupExtension
+    {
+        #region Properties
+
+        /// <summary>
+        /// Тип перечисления.
+        /// </summary>
+        public Type EnumType { get; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Создает экземпляр <see cref="EnumValuesExtension"/>.
+        /// </summary>
+        /// <param name="enumType"> Тип перечисления, значения которого необходимо
+        /// получить.</param>
+        public EnumValuesExtension(Type enumType) => EnumType = enumType;
+
+        #endregion
+
+        #region Methods
+
+        #region MarkupExtension Overriding
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public override object ProvideValue(IServiceProvider serviceProvider) =>
+            Enum.GetValues(EnumType);
+
+        #endregion
+
+        #endregion
+    }
+}
---
+++ src/DeskPlugin/Desk/Extensions/MarkupExtensions/ViewModelsExtension.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace Desk.Extensions.MarkupExtensions
+{
+    /// <summary>
+    /// Класс, предоставляющий экземпляр модели представления по ее типу.
+    /// <para> Наследуется от класса <see cref="MarkupExtension"/> с целью
+    /// использования данного класса непосредственно в разметке окон.</para>
+    /// </summary>
+    internal class ViewModelsExtension : MarkupExtension
+    {
+        #region Properties
+
+        /// <summary>
+        /// Тип модели представления.
+        /// </summary>
+        public Type ViewModelType { get; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Создает экземпляр <see cref="ViewModelsExtension"/>.
+        /// </summary>
+        /// <param name="viewModelType"> Тип модели представления, которую необходимо
+        /// получить.</param>
+        public ViewModelsExtension(Type viewModelType) => 
+            ViewModelType = viewModelType;
+
+        #endregion
+
+        #region Methods
+
+        #region MarkupExtension Overriding
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            // Используем контейнер сервисов плагина, чтобы получить нужную модель
+            // представления.
+            return App.ServiceProvider.GetService(ViewModelType);
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
